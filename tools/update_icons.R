005f7189d197d67a45e472d1f8826a6f28f926c0
seanthimons <thimons.sean@gmail.com>
2025-12-21 13:51:41 -0500

    fix: Update icon update script to automatically fetch the latest release and refresh icon data

--- tools/update_icons.R
+++ tools/update_icons.R
@@ -1,7 +1,10 @@
 library(rvest)
 
 # https://github.com/twbs/icons/releases
-version <- "1.11.1"
+html <- read_html("https://github.com/twbs/icons/releases/latest")
+url <- html_attr(html_element(html, "meta[property='og:url']"), "content")
+version <- sub("^v", "", basename(url))
+
 
 pkg_home <- rprojroot::find_package_root_file()
 pkgload::load_all(pkg_home)
@@ -75,7 +78,11 @@ withr::with_tempdir({
 
   new_icon_info <<- list(
     name = icon_names,
-    contents = vapply(contents, function(x) paste(as.character(x), collapse = "\n"), character(1))
+    contents = vapply(
+      contents,
+      function(x) paste(as.character(x), collapse = "\n"),
+      character(1)
+    )
   )
 })
 
@@ -89,7 +96,8 @@ if (length(missing_icons)) {
   stop(
     "New version of Bootstrap icons dropped the following icons: '",
     paste(missing_icons, collapse = "', '"),
-    "'. Consider keeping them for backwards-compatibility.")
+    "'. Consider keeping them for backwards-compatibility."
+  )
 }
 
 
